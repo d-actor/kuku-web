e3cdc15507a0a550b0f925a4b33ec3c4abe9ceb1
Dan Actor <danltactor@gmail.com>
2018-03-16 18:59:23 -0600

    git init

---
+++ client/src/components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import NoMatch from './NoMatch';
+import NavBar from './NavBar';
+import Login from './Login';
+import Register from './Register';
+import Flash from './Flash';
+import Home from './Home';
+import ProtectedRoute from './ProtectedRoute';
+import AuthRoute from './AuthRoute';
+import FetchUser from './FetchUser';
+import { Switch, Route } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Flash />
+        <FetchUser>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <AuthRoute exact path='/login' component={Login} />
+            <AuthRoute exact path='/register' component={Register} />
+            <Route component={NoMatch} />
+          </Switch>
+        </FetchUser>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'api/auth'
+  namespace :api do
+    #API ROUTES SHOULD GO HERE
+  end
+
+  #Do not place any routes below this one
+  get '*other', to: 'static#index'
+end
